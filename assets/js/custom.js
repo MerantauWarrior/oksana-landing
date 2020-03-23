bd5820b05b321dad7d4093bd594babc38702ddf6
Boris <boris1488ss@gmail.com>
2020-03-24 00:44:24 +0200

    calc

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,11 @@
 $( document ).ready(function() {
   //Modal
+  $('.js-modal').click(function (e) {
+    e.preventDefault();
+    var target = $(this).attr('href');
+    $('.modal').show();
+    $(target).show();
+  });
   document.getElementsByClassName('modal')[0].addEventListener('click', function (event) {
     if (event.target === document.getElementsByClassName('modal')[0]) {
       $('.modal').hide();
@@ -9,12 +15,16 @@ $( document ).ready(function() {
     $('.modal').hide();
   });
   //Caclulator
+  var percent = 8, srok, sum, payment_type = 1;
   function calculate() {
-    if (payment_type == 2) {
-      monthly = ((sum/100*percent)*srok)/(srok*12);
+    if (payment_type === 2) {
+      monthly = parseInt(((sum/100*percent)*srok)/(srok*12));
     } else {
-      monthly = (((sum/100*percent)*srok)+sum)/(srok*12);
+      monthly = parseInt((((sum/100*percent)*srok)+sum)/(srok*12));
     }
+    $('.calculator__amount-value span').text(monthly);
+    $('.credit-rub').text(sum + ' рублей');
+    $('.avans-rub').text(sum/10 + ' рублей');
   }
   $('.sum').rangeslider({
     polyfill: false,
@@ -23,15 +33,24 @@ $( document ).ready(function() {
     handleClass: 'calculator-slider__thumb',
     onInit: function() {
       $('#fSum').val($(this)[0].value);
+      sum = $(this)[0].value;
     },
     onSlide: function(position, value) {
       $('#fSum').val(value);
+      sum = value;
+      calculate();
     }
+  });
+  $('.calculator-radio__item label').click(function () {
+    payment_type = parseInt($(this).siblings('input[name="creditType"]').val());
+    calculate();
   });
   $('#fSum').on('input', function () {
     var value = $(this).val();
     if(value !== ''){
       $('.sum').val(value).change();
+      sum = value;
+      calculate();
     }
   });
   $('.date').rangeslider({
@@ -41,15 +60,20 @@ $( document ).ready(function() {
     handleClass: 'calculator-slider__thumb',
     onInit: function() {
       $('#fDate').val($(this)[0].value);
+      srok = $(this)[0].value;
     },
     onSlide: function(position, value) {
       $('#fDate').val(value);
+      srok = value;
+      calculate();
     }
   });
   $('#fDate').on('input', function () {
     var value = $(this).val();
     if(value !== ''){
       $('.date').val(value).change();
+      srok = value;
+      calculate();
     }
   });
   //PhoneMask
